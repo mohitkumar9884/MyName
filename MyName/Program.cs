9ac2a52baebf5e57bc935213c5c4e5677bc5ef85
mohitkumar9884 <mohitkumar9884@gmail.com>
2023-06-27 13:59:49 +0530

    [mohitkumar9884  adiing Welcome message]

---
+++ MyName/Program.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("My Name Is Mohit");
+        Console.WriteLine("Everyone Welcome To Fellowship");
+
+    }
+}
